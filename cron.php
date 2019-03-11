d8b312744f97936c58f2b7a0bb0e2920d9094a3d
joey <gxlclizongjun@163.com>
2019-03-11 16:59:21 +0800

    112

--- cron.php
+++ cron.php
@@ -21,13 +21,18 @@ if (empty($domainList)) {
 foreach ($domainList as $item) {
     usleep(2500000);
     if (false === domainCheck($apiToken, $item['name'])) {
-        $sql  = "UPDATE `system_domain` SET `status`=:name WHERE `id`=:id";
-        $stmt = $mysql->prepare($sql);
-        $stmt->execute(array(':name' => '0', ':id' => $item['id']));
-        if ($stmt->rowCount() > 0) {
-            file_put_contents('domain.log','域名状态更新成功 '.json_encode($item).'  '.date('Y-m-d H:i:s').PHP_EOL,FILE_APPEND);
-        } else {
-            file_put_contents('domain.log','域名状态更新失败 '.json_encode($item).' '.date('Y-m-d H:i:s').PHP_EOL,FILE_APPEND);
+
+        $sql = "UPDATE `system_domain` SET `status`=:status WHERE `id`=:id";
+
+        $data = array(':status' => '0', ':id' => $item['id']);
+        updateMysql($mysql, $sql, $item, $data, '域名');
+
+        $sql  = "UPDATE `system_app` SET `status`=:status WHERE `bind_domain_ld`=:bind_domain_ld";
+        $data = array(':status' => '0', ':bind_domain_ld' => $item['name']);
+        $result = updateMysql($mysql, $sql, $item, $data, '公众号');
+        if ($result === true) {
+            unlink(__DIR__.'/jssdkphpversion/jsapi_ticket.php');
+            unlink(__DIR__.'/jssdkphpversion/access_token.php');
         }
     }
 }
@@ -36,7 +41,7 @@ foreach ($domainList as $item) {
 /**
  * @param $mysql  mysql资源链接
  * @param $sql    sql语句
- * return mix
+ * @return mix
  */
 function getDataFromMysql($mysql, $sql)
 {
@@ -60,10 +65,35 @@ function getDataFromMysql($mysql, $sql)
     return $data;
 }
 
+/**
+ * @param $mysql
+ * @param $sql
+ * @param $item
+ * @param $data
+ * @param $desc
+ * @return bool
+ */
+function updateMysql($mysql, $sql, $item, $data, $desc)
+{
+    if (empty($mysql) || empty($sql)) {
+        return false;
+    }
+
+    $stmt = $mysql->prepare($sql);
+    $stmt->execute($data);
+    if ($stmt->rowCount() > 0) {
+        file_put_contents('domain.log', $desc . '状态更新成功 ' . json_encode($item) . ' ' . date('Y-m-d H:i:s') . PHP_EOL, FILE_APPEND);
+        return true;
+    } else {
+        file_put_contents('domain.log', $desc . '状态更新失败 ' . json_encode($item) . ' ' . date('Y-m-d H:i:s') . PHP_EOL, FILE_APPEND);
+        return false;
+    }
+}
+
 /** 微信域名接口检测
  * @param $apiToken  您的 API Token，在用户中心可查询到
  * @param $reqUrl    需要检测的地址或域名
- * return code    返回码    9900:正常 | 9904:被封 | 9999:系统错误 | 139:token错误或无权限 | 402:超过调用频率  msg    错误消息    返回的错误消息
+ * @return code    返回码    9900:正常 | 9904:被封 | 9999:系统错误 | 139:token错误或无权限 | 402:超过调用频率  msg    错误消息    返回的错误消息
  */
 function domainCheck($apiToken, $reqUrl)
 {
@@ -75,7 +105,7 @@ function domainCheck($apiToken, $reqUrl)
     $responseArr  = json_decode($responseBody, true);
     if (json_last_error() != JSON_ERROR_NONE) {
         // echo "JSON 解析接口结果出错\n";
-        file_put_contents('ApidomainCheck.log','JSON 解析出错  '.date('Y-m-d H:i:s').PHP_EOL,FILE_APPEND);
+        file_put_contents('ApidomainCheck.log', 'JSON 解析出错  ' . date('Y-m-d H:i:s') . PHP_EOL, FILE_APPEND);
         return 'JSON 解析出错';
     }
     if (isset($responseArr['code'])) {
@@ -84,18 +114,18 @@ function domainCheck($apiToken, $reqUrl)
             //file_put_contents('ApidomainCheck.log',$reqUrl.'域名正常  '.date('Y-m-d H:i:s').PHP_EOL,FILE_APPEND);
             return true;
         } else if ($responseArr['code'] == '9904') {
-            file_put_contents('ApidomainCheck.log',$reqUrl.'域名被封了  '.date('Y-m-d H:i:s').PHP_EOL,FILE_APPEND);
+            file_put_contents('ApidomainCheck.log', $reqUrl . '域名被封了  ' . date('Y-m-d H:i:s') . PHP_EOL, FILE_APPEND);
             return false;
         } else if ($responseArr['code'] == '139') {
-            file_put_contents('ApidomainCheck.log','用户没有权限  '.date('Y-m-d H:i:s').PHP_EOL,FILE_APPEND);
+            file_put_contents('ApidomainCheck.log', '用户没有权限  ' . date('Y-m-d H:i:s') . PHP_EOL, FILE_APPEND);
             return '用户没有权限';
         } else if ($responseArr['code'] == '402') {
-            file_put_contents('ApidomainCheck.log','频率过快  '.date('Y-m-d H:i:s').PHP_EOL,FILE_APPEND);
+            file_put_contents('ApidomainCheck.log', '频率过快  ' . date('Y-m-d H:i:s') . PHP_EOL, FILE_APPEND);
             return '频率过快';
         }
     } else {
         // printf("接口异常：%s\n", var_export($responseArr, true));
-        file_put_contents('ApidomainCheck.log','api error  '.date('Y-m-d H:i:s').PHP_EOL,FILE_APPEND);
+        file_put_contents('ApidomainCheck.log', 'api error  ' . date('Y-m-d H:i:s') . PHP_EOL, FILE_APPEND);
         return 'api error';
     }
 }
