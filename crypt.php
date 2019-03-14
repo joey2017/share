44391dbb2b7fb46f3193004a9d15607fe0bd11a9
joey <gxlclizongjun@163.com>
2019-03-14 18:13:39 +0800

    加密

---
+++ crypt.php
@@ -0,0 +1,96 @@
+<?php
+
+/**
+ * 参数加密类
+ */
+
+class Urlencry
+{
+    public static $encrypt_key = null;  //加密的密钥
+
+    public function __construct($key)
+    {
+        $this->encrypt_key = $key;
+    }
+
+    //密钥处理
+    public static function keyED($txt)
+    {
+        $encrypt_key = self::$encrypt_key;
+        $encrypt_key = md5($encrypt_key);
+        $ctr         = 0;
+        $tmp         = "";
+        for ($i = 0; $i < strlen($txt); $i++) {
+            if ($ctr == strlen($encrypt_key))
+                $ctr = 0;
+            $tmp .= substr($txt, $i, 1) ^ substr($encrypt_key, $ctr, 1);
+            $ctr++;
+        }
+        return $tmp;
+    }
+
+    //地址加密
+    public static function encrypt($txt)
+    {
+        $encrypt_key = md5(mt_rand(0, 100));
+        $ctr         = 0;
+        $tmp         = "";
+        for ($i = 0; $i < strlen($txt); $i++) {
+            if ($ctr == strlen($encrypt_key))
+                $ctr = 0;
+            $tmp .= substr($encrypt_key, $ctr, 1) . (substr($txt, $i, 1) ^ substr($encrypt_key, $ctr, 1));
+            $ctr++;
+        }
+        return self::keyED($tmp);
+    }
+
+    //地址解密
+    public static function decrypt($txt)
+    {
+        $txt = self::keyED($txt);
+        $tmp = "";
+        for ($i = 0; $i < strlen($txt); $i++) {
+            $md5 = substr($txt, $i, 1);
+            $i++;
+            $tmp .= (substr($txt, $i, 1) ^ $md5);
+        }
+        return $tmp;
+    }
+
+    //调用加密链接
+    public static function encrypt_url($url)
+    {
+        $url = self::encrypt($url);
+        return rawurlencode(base64_encode($url));
+    }
+
+    //调用解密链接
+    public static function decrypt_url($url)
+    {
+        return self::decrypt(base64_decode(rawurldecode($url)));
+    }
+
+    public static function geturl($str)
+    {
+        $vars      = array();
+        $str       = self::decrypt_url($str);
+        $url_array = explode('&', $str);
+        if (is_array($url_array)) {
+            foreach ($url_array as $var) {
+                $var_array           = explode("=", $var);
+                $vars[$var_array[0]] = $var_array[1];
+            }
+        }
+        return $vars;
+    }
+}
+
+//=======调用示例========//
+//echo Urlencry::encrypt_url('?a=c&b=fadasf&c=12345555');
+
+//echo '<br>';
+
+//echo Urlencry::decrypt_url('VT4AMg07DGtcdlAxAW5WYQ1tXTYMPFt6BDICdAo0Vm9VMAFgXWUNPQgxBDIIbwNn');
+
+//========调用示例=======//
+
