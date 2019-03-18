4f7338339951c43e72be876a0d9aba4d0aa9c0bb
joey <gxlclizongjun@163.com>
2019-03-18 18:22:28 +0800

    1

---
+++ html/js/s4.js
@@ -0,0 +1,8 @@
+arr_wx=['xxasec'];
+
+var wx_index = Math.floor((Math.random()*arr_wx.length));
+var stxlwx = arr_wx[wx_index];
+var wx = ""+arr_wx[wx_index];
+var wximg = "<img width='220' height='220' src=index_files/images/" + stxlwx + ".jpg />";
+var wximgaes = "<img width='160' height='160' src=index_files/images/" + stxlwx + ".jpg />";
+ var wx_img = "<img width='165' src='images/ewm/"+img+"'>";
---
+++ html/js/wx.js
@@ -0,0 +1,6 @@
+arr_wx = ['ccaveb','ssaved','TJXXX888','xxaseb','xxased'];
+var wx_index = Math.floor((Math.random()*arr_wx.length));
+var stxlwx = arr_wx[wx_index];
+var wx_img_bottom ="<img src='" + "index_files/images/" + stxlwx +".jpg'" +" " + "/>";
+var wx_img = "<img style='width: 250px;'" +" "+ "src='" + "index_files/images/" + stxlwx +".jpg'" +" " + "/>";
+
